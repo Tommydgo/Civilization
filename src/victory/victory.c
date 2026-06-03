71f47fd8800a12f2f34d230c0798c425340ff9cd
Tommydgo <tommy.diago@epitech.eu>
2026-06-03 14:48:26 +0000

    feat: Refactor victory conditions to streamline winner determination and improve code organization

--- src/victory/victory.c
+++ src/victory/victory.c
@@ -1,6 +1,7 @@
 #include "victory/victory.h"
 #include "victory/score.h"
 #include "empire/religion.h"
+#include "world/map.h"
 
 bool victory_check_science(GameState *gs)
 {
@@ -11,15 +12,12 @@ bool victory_check_science(GameState *gs)
 
 bool victory_check_military(GameState *gs)
 {
-    
     if (gs->ai_factions.count == 0)
         return false;
-    
     for (int i = 0; i < gs->ai_factions.count; i++) {
         if (!gs->ai_factions.data[i].is_eliminated)
             return false;
     }
-    
     for (int i = 0; i < gs->cities.count; i++) {
         if (gs->cities.data[i].is_active
                 && gs->cities.data[i].owner == PLAYER_OWNER_ID)
@@ -38,53 +36,67 @@ bool victory_check_score(GameState *gs)
     return gs->current_turn >= gs->config.max_turns;
 }
 
+static int winner_science(GameState *gs)
+{
+    (void)gs;
+    return PLAYER_OWNER_ID;
+}
+
+static int winner_military(GameState *gs)
+{
+    int i = 0;
+
+    while (i < gs->cities.count) {
+        if (gs->cities.data[i].is_active)
+            return gs->cities.data[i].owner;
+        i++;
+    }
+    return PLAYER_OWNER_ID;
+}
+
+static int winner_religion(GameState *gs)
+{
+    int non_water = map_count_non_water(gs);
+    int i = 0;
+
+    while (i < gs->religions.count) {
+        int tiles = gs->religions.data[i].converted_tiles;
+        if (non_water > 0 && tiles * 100 / non_water >= RELIGION_VICTORY_THRESHOLD)
+            return gs->religions.data[i].founder_owner;
+        i++;
+    }
+    return PLAYER_OWNER_ID;
+}
+
+typedef struct {
+    VictoryType type;
+    bool        (*check)(GameState *);
+    int         (*winner)(GameState *);
+} VictoryEntry;
+
+static const VictoryEntry VICTORIES[] = {
+    { VICTORY_SCIENCE,  victory_check_science,  winner_science    },
+    { VICTORY_MILITARY, victory_check_military, winner_military   },
+    { VICTORY_RELIGION, victory_check_religion, winner_religion   },
+    { VICTORY_SCORE,    victory_check_score,    score_find_winner },
+};
+
+static const int VICTORY_COUNT = 4;
+
 void victory_check(GameState *gs)
 {
+    int i = 0;
+
     if (gs->victory.achieved)
         return;
-
-    if (victory_check_science(gs)) {
-        gs->victory.achieved = true;
-        gs->victory.type = VICTORY_SCIENCE;
-        gs->victory.winner_owner = PLAYER_OWNER_ID;
-        gs->game_over = true;
-        return;
-    }
-
-    if (victory_check_military(gs)) {
-        for (int i = 0; i < gs->cities.count; i++) {
-            if (gs->cities.data[i].is_active) {
-                gs->victory.achieved = true;
-                gs->victory.type = VICTORY_MILITARY;
-                gs->victory.winner_owner = gs->cities.data[i].owner;
-                gs->game_over = true;
-                return;
-            }
+    while (i < VICTORY_COUNT) {
+        if (VICTORIES[i].check(gs)) {
+            gs->victory.achieved = true;
+            gs->victory.type = VICTORIES[i].type;
+            gs->victory.winner_owner = VICTORIES[i].winner(gs);
+            gs->game_over = true;
+            return;
         }
-    }
-
-    if (victory_check_religion(gs)) {
-        int non_water = 0;
-        for (int y = 0; y < gs->map.height; y++)
-            for (int x = 0; x < gs->map.width; x++)
-                if (gs->map.grid[y][x].type != TERRAIN_WATER)
-                    non_water++;
-        for (int i = 0; i < gs->religions.count; i++) {
-            int tiles = gs->religions.data[i].converted_tiles;
-            if (non_water > 0 && tiles * 100 / non_water >= RELIGION_VICTORY_THRESHOLD) {
-                gs->victory.achieved = true;
-                gs->victory.type = VICTORY_RELIGION;
-                gs->victory.winner_owner = gs->religions.data[i].founder_owner;
-                gs->game_over = true;
-                return;
-            }
-        }
-    }
-
-    if (victory_check_score(gs)) {
-        gs->victory.achieved = true;
-        gs->victory.type = VICTORY_SCORE;
-        gs->victory.winner_owner = score_find_winner(gs);
-        gs->game_over = true;
+        i++;
     }
 }
