fc7ac0989f950546049d122ad3012f887b7427e9
Tommydgo <tommy.diago@epitech.eu>
2026-06-04 21:13:36 +0000

    ui: update draw/dialog headers for Raylib (remove window/font pointers)

--- src/ui/ui_dialog.h
+++ src/ui/ui_dialog.h
@@ -1,9 +1,10 @@
 #ifndef UI_DIALOG_H
 #define UI_DIALOG_H
-#include <SFML/Graphics.h>
-void dialog_text_input(sfRenderWindow *win, sfFont *font,
-                       const char *prompt, char *buf, int len);
-int  dialog_list_select(sfRenderWindow *win, sfFont *font,
-                        const char *title, const char **items,
+
+#include <raylib.h>
+
+void dialog_text_input(Font font, const char *prompt, char *buf, int len);
+int  dialog_list_select(Font font, const char *title, const char **items,
                         int count, int default_sel);
+
 #endif
--- src/ui/ui_draw.h
+++ src/ui/ui_draw.h
@@ -1,14 +1,14 @@
 #ifndef UI_DRAW_H
 #define UI_DRAW_H
-#include <SFML/Graphics.h>
+
+#include <raylib.h>
 #include "structs.h"
 #include "ui/ui_layout.h"
 
-void draw_toolbar(sfRenderWindow *win, sfFont *font, GameState *gs);
-void draw_map(sfRenderWindow *win, sfFont *font, GameState *gs,
-              int selected_id, int mode);
-void draw_panel(sfRenderWindow *win, sfFont *font, GameState *gs,
-                int selected_id, int mode);
-void draw_log(sfRenderWindow *win, sfFont *font, GameState *gs,
-              int info_count, const char info_buf[][96]);
+void draw_toolbar(Font font, GameState *gs);
+void draw_map(Font font, GameState *gs, int selected_id, int mode);
+void draw_panel(Font font, GameState *gs, int selected_id, int mode);
+void draw_log(Font font, GameState *gs, int info_count,
+              const char info_buf[][96]);
+
 #endif
